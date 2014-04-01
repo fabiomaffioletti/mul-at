d170cada91df415fa81f223c21cad1572ec456ea
fabio <fabio@skebby.it>
2014-04-01 14:22:20 +0200

    clean config classes

--- src/main/java/com/at/mul/CustomerConfig.java
+++ src/main/java/com/at/mul/CustomerConfig.java
@@ -23,13 +23,6 @@ public class CustomerConfig {
 	
 	@Bean(name = "customerDataSource", initMethod = "init", destroyMethod = "close")
     public DataSource customerDataSource() {
-//        MysqlXADataSource mysqlXaDataSource = new MysqlXADataSource();
-//        mysqlXaDataSource.setUrl("jdbc:mysql://localhost:3306/atomikos_1");
-//		mysqlXaDataSource.setPinGlobalTxToPhysicalConnection(true);
-//		mysqlXaDataSource.setPassword("password");
-//		mysqlXaDataSource.setUser("root");
-//		mysqlXaDataSource.setPinGlobalTxToPhysicalConnection(true);
-		
 		JdbcDataSource h2XaDataSource = new JdbcDataSource();
 		h2XaDataSource.setURL("jdbc:h2:customer");
 		
@@ -47,9 +40,6 @@ public class CustomerConfig {
 		entityManager.setJpaVendorAdapter(jpaVendorAdapter);
 		entityManager.setPackagesToScan("com.at.mul.domain.customer");
 		entityManager.setPersistenceUnitName("customerPersistenceUnit");
-//		Properties properties = new Properties();
-//		properties.put("javax.persistence.transactionType", "JTA");
-//		entityManager.setJpaProperties(properties);
 		return entityManager;
 	}
 
--- src/main/java/com/at/mul/OrderConfig.java
+++ src/main/java/com/at/mul/OrderConfig.java
@@ -23,13 +23,6 @@ public class OrderConfig {
 	
 	@Bean(name = "orderDataSource", initMethod = "init", destroyMethod = "close")
     public DataSource orderDataSource() {
-//        MysqlXADataSource mysqlXaDataSource = new MysqlXADataSource();
-//        mysqlXaDataSource.setUrl("jdbc:mysql://localhost:3306/atomikos_2");
-//		mysqlXaDataSource.setPinGlobalTxToPhysicalConnection(true);
-//		mysqlXaDataSource.setPassword("password");
-//		mysqlXaDataSource.setUser("root");
-//		mysqlXaDataSource.setPinGlobalTxToPhysicalConnection(true);
-		
 		JdbcDataSource h2XaDataSource = new JdbcDataSource();
 		h2XaDataSource.setURL("jdbc:h2:order");
 
@@ -47,9 +40,6 @@ public class OrderConfig {
 		entityManager.setJpaVendorAdapter(jpaVendorAdapter);
 		entityManager.setPackagesToScan("com.at.mul.domain.order");
 		entityManager.setPersistenceUnitName("orderPersistenceUnit");
-//		Properties properties = new Properties();
-//		properties.put("javax.persistence.transactionType", "JTA");
-//		entityManager.setJpaProperties(properties);
 		return entityManager;
 	}
 
